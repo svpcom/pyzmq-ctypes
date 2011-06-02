385cdbae7f2d34629cd00f73297486fa526a85f8
Vasiliy Evseenko <vasiliy.evseenko@qik.com>
2011-06-02 09:49:54 +0400

    Bump version to 2.1.3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ pyzmq-ctypes is a ctypes binding for the ZeroMQ library
 
 setup(
     name = "pyzmq-ctypes",
-    version = "2.1.2",
+    version = "2.1.3",
     packages = ['zmq', 'zmq.core'],
     author = "Daniel Holth",
     author_email = "dholth@fastmail.fm",
